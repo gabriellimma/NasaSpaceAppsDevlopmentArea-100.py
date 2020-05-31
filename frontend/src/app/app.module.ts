f10d1a61ad6c0bf78a2342a105456f1ebff65fad
Bielzin42 <nickspoot@gmail.com>
2020-05-31 15:54:03 -0300

    implementing aboutus page

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/template/header/header.component';
 import { FooterComponent } from './components/template/footer/footer.component';
 import { HomeComponent } from './components/template/home/home.component';
+import { AboutComponent } from './components/template/about/about.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     FooterComponent,
-    HomeComponent
+    HomeComponent,
+    AboutComponent
   ],
   imports: [
     BrowserModule,
